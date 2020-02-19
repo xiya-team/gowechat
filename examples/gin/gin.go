ab87e7aac19e2746d98678e0e7e3cad39c873c79
yangcuiwang <yangcuiwang@qq.com>
2020-02-19 10:45:52 +0800

    init

---
+++ examples/gin/gin.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/xiya-team/gowechat"
+	"github.com/xiya-team/gowechat/mp/message"
+	"github.com/xiya-team/gowechat/wxcontext"
+	"gowechat"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.Any("/", hello)
+	router.Run(":8001")
+}
+
+func hello(c *gin.Context) {
+
+	//配置微信参数
+	config := wxcontext.Config{
+		AppID:          "your app id",
+		AppSecret:      "your app secret",
+		Token:          "your token",
+		EncodingAESKey: "your encoding aes key",
+	}
+	wc := gowechat.NewWechat(config)
+
+	mp, err := wc.MpMgr()
+	if err != nil {
+		return
+	}
+
+	// 传入request和responseWriter
+	msgHandler := mp.GetMsgHandler(c.Request, c.Writer)
+	//设置接收消息的处理方法
+	msgHandler.SetHandleMessageFunc(func(msg message.MixMessage) *message.Reply {
+
+		//回复消息：演示回复用户发送的消息
+		text := message.NewText(msg.Content)
+		return &message.Reply{message.MsgTypeText, text}
+	})
+
+	//处理消息接收以及回复
+	err = msgHandler.Handle()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
---
+++ mch.go
@@ -0,0 +1,21 @@
+package gowechat
+
+import (
+	"gowechat/mch/pay"
+	"gowechat/mch/paytool"
+)
+
+//MchMgr mch mgt
+type MchMgr struct {
+	*Wechat
+}
+
+// GetPay 基本支付api
+func (wc *MchMgr) GetPay() *pay.Pay {
+	return pay.NewPay(wc.Context)
+}
+
+// GetPayTool 支付工具，发红包等
+func (wc *MchMgr) GetPayTool() *paytool.PayTool {
+	return paytool.NewPayTool(wc.Context)
+}
